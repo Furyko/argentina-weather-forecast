058dda987b00a639aa9b1d05a41d2a253fca7739
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2024-01-19 21:04:35 -0300

    Add dotenv dependency
    Updated api for forecast
    
    Updated forecast

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -4,88 +4,58 @@ import '../styles/styles.css';
 
 function WeatherForecast() {
     var [forecast, setForecast] = useState({});
-    var [forecastTomorrow, setForecastTomorrow] = useState({weather: {morning_temp: ''}});
-    var [forecastTwoDays, setForecastTwoDays] = useState({weather: {morning_temp: ''}});
-    var [forecastThreeDays, setForecastThreeDays] = useState({weather: {morning_temp: ''}});
+
     useEffect(() => {
-        axios.get('https://ws.smn.gob.ar/map_items/forecast/1')
-            .then(function (response) {
-                for (let x in response.data) {
-                    if (response.data[x].name === 'Presidencia Roque Sáenz Peña' && response.data[x].province === 'Chaco') {
-                        if (response.data[x].weather.day !== forecastTomorrow.weather.day) {
-                            setForecastTomorrow(response.data[x]);
-                        }
-                        break;
-                    }
-                 }
+        axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/8127?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}`)
+            .then(function (res) {
+                if (res.data) setForecast(res.data);
+                console.log(res.data)
             })
-        axios.get('https://ws.smn.gob.ar/map_items/forecast/2')
-            .then(function (response) {
-                for (let x in response.data) {
-                    if (response.data[x].name === 'Presidencia Roque Sáenz Peña' && response.data[x].province === 'Chaco') {
-                        if (response.data[x].weather.day !== forecastTwoDays.weather.day) {
-                            setForecastTwoDays(response.data[x]);
-                        }
-                        break;
-                    }
-                 }
-            })
-        axios.get('https://ws.smn.gob.ar/map_items/forecast/3')
-            .then(function (response) {
-                for (let x in response.data) {
-                    if (response.data[x].name === 'Presidencia Roque Sáenz Peña' && response.data[x].province === 'Chaco') {
-                        if (response.data[x].weather.day !== forecastThreeDays.weather.day) {
-                            setForecastThreeDays(response.data[x]);
-                        }
-                        break;
-                    }
-                 }
-            })
-        setForecast({forecastTomorrow, forecastTwoDays, forecastThreeDays});
-    }, [forecastTomorrow, forecastTwoDays, forecastThreeDays])
+    }, [])
+
+    /* Utilities */
+    const getDayName = (dateString) => {
+        const days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+        let d = new Date(dateString);
+        let dayName = days[d.getDay()];
+        return dayName
+    }
+
+    const convertFtoC = (fahrenheit) => {
+        let celsius = (fahrenheit - 32) * 5/9;
+        return celsius.toFixed();
+    }
+
+    const getTemperature = (data) => {
+        if (data.Unit == 'F') {
+            return `${ convertFtoC(data.Value) }°C`
+        }
+        return `${data.Value}°${data.Unit}`
+    }
+
     return (
         <div className='container mb-4'>
-            {forecast.forecastTomorrow && forecast.forecastTwoDays && forecast.forecastThreeDays ?
+            { forecast.DailyForecasts ?
             <div className='row'>
-                <div className='col mt-4'>
-                    <div className='card' style={{minWidth: "200px"}}>
-                        <div className='card-header d-flex justify-content-center'>Mañana</div>
-                        <div className='card-body'>
-                            <p className='d-flex justify-content-center'>Por la mañana</p>
-                            <p className='d-flex justify-content-center font-size-forecast-temp'>{forecast.forecastTomorrow.weather.morning_temp}ºC</p>
-                            <p>{forecast.forecastTomorrow.weather.morning_desc}</p>
-                            <p className='d-flex justify-content-center'>Por la tarde</p>
-                            <p className='d-flex justify-content-center font-size-forecast-temp'>{forecast.forecastTomorrow.weather.afternoon_temp}ºC</p>
-                            <p>{forecast.forecastTomorrow.weather.afternoon_desc}</p>
+                { forecast.DailyForecasts.map((day, index) => 
+                    <div className='col mt-4' key={index}>
+                        <div className='card' style={{minWidth: "200px"}}>
+                            <div className='card-header d-flex justify-content-center'>{ getDayName(day.Date) }</div>
+                            <div className='card-body'>
+                                <div className='d-flex justify-content-around'>
+                                    <p className='d-flex justify-content-center font-size-forecast-temp'>{ getTemperature(day.Temperature.Minimum) }</p>
+                                    <p className='d-flex justify-content-center font-size-forecast-temp'>{ getTemperature(day.Temperature.Maximum) }</p>
+                                </div>
+                                <p className='d-flex justify-content-center'>Por el día</p>
+                                <p>{ day.Day.IconPhrase }</p>
+                                <p>{ day.Day.HasPrecipitation ? 'Con presipitaciones' : 'Sin precipitaciones'}</p>
+                                <p className='d-flex justify-content-center'>Por la noche</p>
+                                <p>{ day.Night.IconPhrase }</p>
+                                <p>{ day.Night.HasPrecipitation ? 'Con presipitaciones' : 'Sin precipitaciones'}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='col mt-4'>
-                    <div className='card' style={{minWidth: "200px"}}>
-                        <div className='card-header d-flex justify-content-center'>En dos días</div>
-                        <div className='card-body'>
-                            <p className='d-flex justify-content-center'>Por la mañana</p>
-                            <p className='d-flex justify-content-center font-size-forecast-temp'>{forecast.forecastTwoDays.weather.morning_temp}ºC</p>
-                            <p>{forecast.forecastTwoDays.weather.morning_desc}</p>
-                            <p className='d-flex justify-content-center'>Por la tarde</p>
-                            <p className='d-flex justify-content-center font-size-forecast-temp'>{forecast.forecastTwoDays.weather.afternoon_temp}ºC</p>
-                            <p>{forecast.forecastTwoDays.weather.afternoon_desc}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='col mt-4'>
-                    <div className='card' style={{minWidth: "200px"}}>
-                        <div className='card-header d-flex justify-content-center'>En tres días</div>
-                        <div className='card-body'>
-                            <p className='d-flex justify-content-center'>Por la mañana</p>
-                            <p className='d-flex justify-content-center font-size-forecast-temp'>{forecast.forecastThreeDays.weather.morning_temp}ºC</p>
-                            <p>{forecast.forecastThreeDays.weather.morning_desc}</p>
-                            <p className='d-flex justify-content-center'>Por la tarde</p>
-                            <p className='d-flex justify-content-center font-size-forecast-temp'>{forecast.forecastThreeDays.weather.afternoon_temp}ºC</p>
-                            <p>{forecast.forecastThreeDays.weather.afternoon_desc}</p>
-                        </div>
-                    </div>
-                </div>
+                ) }
             </div> : 
             <div></div>}
         </div>
