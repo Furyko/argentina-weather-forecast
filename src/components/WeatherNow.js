d41f75d4d493e10dfef00b5fe2b001bf15ae1777
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2024-01-20 01:46:53 -0300

    Update current weather endpoint
    Add new current weather info

--- src/components/WeatherNow.js
+++ src/components/WeatherNow.js
@@ -7,7 +7,7 @@ function WeatherNow({ locationData }) {
     const [weatherNow, setWeatherNow] = useState([]);
 
     useEffect(() => (
-        axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${locationData.Key}?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}`)
+        axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${locationData.Key}?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}&details=true`)
          .then((res) => {
             const data = res.data[0]
             console.log(data)
@@ -24,8 +24,13 @@ function WeatherNow({ locationData }) {
                     <div>
                         <span className='font-size-location'>{ locationData.LocalizedName }</span>
                         <p className='font-size-temp'>{weatherNow.Temperature.Metric.Value}°{weatherNow.Temperature.Metric.Unit}</p>
+                        <span className='font-size-description'>ST: { weatherNow.RealFeelTemperatureShade.Metric.Value }°{ weatherNow.RealFeelTemperatureShade.Metric.Unit }</span>
                         <p className='font-size-description'>{weatherNow.WeatherText}</p>
-                        <p className='font-size-weather'>{ weatherNow.HasPrecipitation ? 'Sin precipitaciones' : 'Con precipitaciones' }</p>
+                        <p className='font-size-weather'>{ weatherNow.HasPrecipitation ? 'Con precipitaciones' : 'Sin precipitaciones' }</p>
+                        <p className='font-size-weather'>Humedad: { weatherNow.RelativeHumidity }</p>
+                        <p className='font-size-weather'>Viento: { weatherNow.Wind.Speed.Metric.Value } { weatherNow.Wind.Speed.Metric.Unit } { weatherNow.Wind.Direction.Degrees } { weatherNow.Wind.Direction.Localized }</p>
+                        <p className='font-size-weather'>Ráfagas: { weatherNow.WindGust.Speed.Metric.Value } { weatherNow.WindGust.Speed.Metric.Unit }</p>
+                        <p className='font-size-weather'>Visibilidad: { weatherNow.Visibility.Metric.Value } { weatherNow.Visibility.Metric.Unit }</p>
                     </div>
                 </div>
             </div> : <p className='d-flex justify-content-center font-size-description'>Cargando...</p>}
