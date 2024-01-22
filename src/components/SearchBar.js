ef957628fec4370f0f158fe859582897417431ae
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2024-01-21 22:25:33 -0300

    Add alert to search bar for searchs with no results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,23 +4,45 @@ import { LocationDataContext } from '../pages/HomePage';
 
 function SearchBar() {
     const [ cityName, setCityName ] = useState()
+    const [ cityStaticName, setCityStaticName ] = useState()
+    const [ showAlert, setShowAlert ] = useState(false)
     const { locationData, setLocationData } = useContext(LocationDataContext)
 
     const onSearchAction = () => {
         console.log("city: ",cityName)
+        setCityStaticName(cityName)
         axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}&q=${cityName}&details=true`)
          .then((res) => {
             console.log(res.data)
-            console.log("key",res.data[0].Key)
+            console.log("key",res.data[0]?.Key)
+            console.log(res.data.length)
+            if (res.data.length <= 0) {
+                setShowAlert(true)
+            } else { 
+                setShowAlert(false)
+            }
             setLocationData(res.data[0])
          })
     }
 
+    const onPressAlertButton = () => {
+        setShowAlert(false)
+    }
+
     return (
         <div className={"container d-flex align-items-center " + (locationData ? "mt-4" : "vh-100")}>
-            <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Busca una ciudad (Ejemplo: San Miguel, Buenos Aires)" onChange={(e) => setCityName(e.target.value)}/>
-                <button className="btn btn-primary" type="button" onClick={onSearchAction}>Buscar</button>
+            <div className='input-group mb-3 justify-content-center'>
+                <div className="input-group">
+                    <input type="text" className="form-control" placeholder="Busca una ciudad (Ejemplo: San Miguel, Buenos Aires)" onChange={(e) => setCityName(e.target.value)}/>
+                    <button className="btn btn-primary" type="button" onClick={onSearchAction}>Buscar</button>
+                </div> 
+                { showAlert ?
+                    <div className={"alert alert-warning alert-dismissible fade mt-3 show"}>
+                        No se encontró una ciudad llamada "{cityStaticName}"
+                        <button type="button" className="btn-close" onClick={onPressAlertButton}></button>
+                    </div> :
+                    <></>
+                }
             </div>
         </div>
     )
