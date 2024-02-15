a56c9cac5bc30a6bd5bc095409843698b8d94fe9
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2024-02-15 01:38:36 -0300

    Add google font
    Update logo on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import logo from '../logo.png';
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <div className="navbar-brand">Tiempo Argentina</div>
+                <div className="navbar-brand">
+                    <img src={logo} alt="" height="26" className="d-inline-block align-text-top"/> Tiempo Argentina
+                </div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
